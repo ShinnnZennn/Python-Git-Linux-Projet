902128d993eac7a0889c8cb5c1a0d5b8e34dda52
Benjamin-projects <marguinbenjamin@gmail.com>
2025-11-27 09:29:07 +0100

    remove app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from single_asset.ui import run as run_single_asset
-
-st.set_page_config(page_title="Linux Git Python Project", layout="wide")
-
-page = st.sidebar.selectbox("Module", ["Single Asset (Quant A)"])
-
-if page == "Single Asset (Quant A)":
-    run_single_asset()
